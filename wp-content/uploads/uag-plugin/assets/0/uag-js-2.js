ee89bedc3e0cba25fe0bee2ddef8ebbfb4c10f5a
xin928 <928ss255718@gmail.com>
2024-08-09 18:34:36 +0800

    xin_0809
    更新圖庫

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-2.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"f0b021f2","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-f0b021f2' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"f0b021f2","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-f0b021f2' );
+});
+ });
